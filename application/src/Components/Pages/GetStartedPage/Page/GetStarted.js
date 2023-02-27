5ddea8918adbfe2b5e7e97f3469ee6d175c728eb
shubhbonde <sbonde66@gmail.com>
2023-02-27 17:25:46 +0530

    second commit

---
+++ application/src/Components/Pages/GetStartedPage/Page/GetStarted.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+import './GetStarted.css'
+import Button from '../button/button'
+
+const GetStarted = () =>{
+    return <div className="gettingStarted_img">
+        
+        <Button/>
+     </div> 
+}
+
+export default GetStarted;
---
+++ application/src/Components/Pages/GetStartedPage/button/button.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+import './button.css'
+
+const Button = () =>{
+    return  <input type="button" value="Get Started"  className='GettingStarted_btn'/>
+        
+    
+}
+export default Button;
